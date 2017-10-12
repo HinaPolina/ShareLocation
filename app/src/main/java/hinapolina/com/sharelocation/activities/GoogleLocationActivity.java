2771949b018995b00613efc103ceb587059abfd1
Hina Khan <happysunshine786@gmail.com>
2017-10-12 07:51:07 -0700

    Initial commit for Google map Added Dependency + API key + Permissions

---
+++ app/src/main/java/hinapolina/com/sharelocation/activities/GoogleLocationActivity.java
@@ -0,0 +1,11 @@
+package hinapolina.com.sharelocation.activities;
+
+import android.app.Application;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hinaikhan on 10/12/17.
+ */
+
+public class GoogleLocationActivity extends AppCompatActivity {
+}
