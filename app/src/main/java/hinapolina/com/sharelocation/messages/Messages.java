be2534f30bb2a2d53007aee9932733f10734d6d2
Hina Khan <happysunshine786@gmail.com>
2017-10-17 00:40:18 -0700

    implement send photo function

--- app/src/main/java/hinapolina/com/sharelocation/messages/Messages.java
+++ app/src/main/java/hinapolina/com/sharelocation/messages/Messages.java
@@ -1,8 +1,14 @@
 package hinapolina.com.sharelocation.messages;
 
+import android.content.Intent;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
@@ -11,14 +17,23 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import hinapolina.com.sharelocation.Application;
+import hinapolina.com.sharelocation.BatteryStatus;
 import hinapolina.com.sharelocation.R;
 import hinapolina.com.sharelocation.adapters.MessageAdapter;
 import hinapolina.com.sharelocation.model.User;
@@ -27,6 +42,8 @@ public class Messages extends AppCompatActivity {
 
     private static final String TAG = Messages.class.getSimpleName();
     private static final int MESSAGE_LENGTH_LIMIT = 500;
+    private static final int RC_PHOTO_PICKER =  2;
+
 
     private ListView mlvMessage;
     private ProgressBar mProgressBar;
@@ -38,8 +55,11 @@ public class Messages extends AppCompatActivity {
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
     private FirebaseAuth mFirebaseAuth;
+    private FirebaseStorage mFirebaseStorage;
+    private StorageReference mFirebasetorageReference;
     private List<User> mUsers = new ArrayList<>();
-
+    private ChildEventListener mChildEventListener;
+    private String mUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +67,14 @@ public class Messages extends AppCompatActivity {
         setContentView(R.layout.activity_messages);
         initView();
 
+        //initialize fb references
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mDatabaseReference = Application.getmDatabase().child("messages");
+        mFirebaseStorage = FirebaseStorage.getInstance();
+        mFirebasetorageReference = mFirebaseStorage.getReference().child("chat_photos");
+
+        //adapter
         mMessageAdapter = new MessageAdapter(this, R.layout.item_message, mUsers);
         mlvMessage.setAdapter(mMessageAdapter);
 
@@ -69,15 +94,6 @@ public class Messages extends AppCompatActivity {
     }
 
     private void onClickListeners(){
-
-        btnSendMessage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO: Send messages on click
-
-
-            }
-        });
 
         //enable send button when there's text to send
         etMessage.addTextChangedListener(new TextWatcher() {
@@ -100,6 +116,95 @@ public class Messages extends AppCompatActivity {
 
             }
         });
+        etMessage.setFilters(new InputFilter[]{new InputFilter.LengthFilter(MESSAGE_LENGTH_LIMIT)});
+
+        //send button send messgae & clears the Edit text
+        btnSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Send messages on click
+               User user = new User();
+               etMessage.setText(user.getText());
+               mDatabaseReference.push().setValue(user);
+
+                //clear the input box
+                etMessage.setText(" ");
+
+            }
+        });
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                User user = dataSnapshot.getValue(User.class);
+                mMessageAdapter.add(user);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+
+        //ImagePickerButton shows an image picker to upload a image for a message
+        imgPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentPhoto = new Intent(Intent.ACTION_GET_CONTENT);
+                intentPhoto.setType("image/jpeg");
+                intentPhoto.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intentPhoto, "Complete action using"), RC_PHOTO_PICKER);
+            }
+        });
+
+        mDatabaseReference.addChildEventListener(mChildEventListener);
 
     }
-}
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == RC_PHOTO_PICKER && resultCode == RESULT_OK) {
+            Uri selectedImageUri = data.getData();
+            // Get a reference to store file at chat_photos/<FILENAME>
+            StorageReference photoRef = mFirebasetorageReference.child(selectedImageUri.getLastPathSegment());
+            // Upload file to Firebase Storage
+            photoRef.putFile(selectedImageUri)
+                .addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // When the image has successfully uploaded, we get its download URL
+                                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                                        // Set the download URL to the message box, so that the user can send it to the database
+                                User user = new User();
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                imgPhotoButton.setImageIcon(Icon.createWithContentUri(downloadUrl.toString()));
+                                mDatabaseReference.push().setValue(user);
+                            }
+
+                        }
+                    });
+                }
+        }
+    }
+
+
+
+
+
+
+
